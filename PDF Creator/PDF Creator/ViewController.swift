adc785286802aaca8da7abf9475176a386f38993
Gilberto Silva <gilberto.silva@sigga.com>
2022-04-11 19:24:37 -0300

    Creating form layout.

--- PDF Creator/PDF Creator/ViewController.swift
+++ PDF Creator/PDF Creator/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    @IBOutlet weak var selectImageLabel: UILabel!
+    @IBOutlet weak var contactInfoTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        applyBorder(textView: bodyTextView)
+        applyBorder(textView: contactInfoTextView)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ViewController.tapFunction))
+        selectImageLabel.addGestureRecognizer(tap)
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    private func applyBorder(textView: UITextView) {
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    @IBAction func onCreateButtonClick(_ sender: UIButton) {
+        
+    }
+    
+    @objc
+    func tapFunction(sender:UITapGestureRecognizer) {
+        print("tap working")
+    }
 }
+
+
 
