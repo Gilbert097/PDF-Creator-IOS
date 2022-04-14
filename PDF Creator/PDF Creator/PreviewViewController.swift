2d42d91aa2f248cd8e550d2dfca2e5f53bf5bb8d
Gilberto Silva <gilberto.silva@sigga.com>
2022-04-14 10:58:11 -0300

    Sharing the PDF.

--- PDF Creator/PDF Creator/PreviewViewController.swift
+++ PDF Creator/PDF Creator/PreviewViewController.swift
@@ -9,7 +9,7 @@ import PDFKit
 import UIKit
 
 class PreviewViewController: UIViewController, PDFViewDelegate  {
-
+    
     let pdfView = PDFView()
     public var documentData: Data?
     
@@ -17,24 +17,33 @@ class PreviewViewController: UIViewController, PDFViewDelegate  {
         super.viewDidLoad()
         view.addSubview(pdfView)
         
-//        guard
-//            let url = Bundle.main.url(forResource: "PDF_2022_03_28_23_24_02", withExtension: "pdf"),
-//            let document = PDFDocument(url: url)
-//        else {
-//            return
-//        }
+        //        guard
+        //            let url = Bundle.main.url(forResource: "PDF_2022_03_28_23_24_02", withExtension: "pdf"),
+        //            let document = PDFDocument(url: url)
+        //        else {
+        //            return
+        //        }
         
         if let data = documentData {
             pdfView.document = PDFDocument(data: data)
             pdfView.autoScales = true
             pdfView.delegate = self
         }
-       
+        
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         pdfView.frame = view.bounds
     }
-
+    
+    @IBAction func onSharedButtonClick(_ sender: UIBarButtonItem) {
+        if let data = documentData {
+            let vc = UIActivityViewController(
+                activityItems: [data],
+                applicationActivities: []
+            )
+            present(vc, animated: true, completion: nil)
+        }
+    }
 }
